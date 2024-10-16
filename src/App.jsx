10dd3536f90939223e83877c87cec1eecf3a9163
kaylathornton55@icloud.com <kayla_thornton@Kaylas-MacBook-Air.local>
2024-10-16 15:42:39 -0400

    Added functionality to the next playlist button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Playlist from './components/Playlist';
-// import getAuth from './components/AuthToken';
+import getAuth from './components/AuthToken';
+
+// gets authorization token
+// console.log(getAuth)
 
 function App() {
   
@@ -14,45 +17,55 @@ function App() {
   // displays playlist owner
   const [playlistOwner, setPlaylistOwner] = useState(null);
   // indexes JSON doc to get the next playlist
-  const [index, setIndex] = useState(5);
+  const [index, setIndex] = useState(0);
+  // stores playlist data
+  const [playlistData, setPlaylistData] = useState([]);
 
-  // funciton displays next playlist in API 
-  const onNextPlaylist = async () => {
+  // makes API call to store playlist data in var playlistData
+  const getPlaylist = async () => {
+    // gets authorization token for API request
+    const apiAuth = "BQBDm0MvIqWP7y6SqionnJSKNDy3Vjzf82SJggmPE6QOT4YkEb65LhJKZXf-TOV0dW1-d1dfCzmDl9YQGeW77DOt4crgJCmhLr9B3SJNWM0AJoKV7ck";
+    console.log(`My API access ${apiAuth}`)
+
+    // Spotify API endpoint to rnb playlists
+    const apiURL = 'https://api.spotify.com/v1/search?q=genre%3AR%26b&type=playlist&limit=25'
+    // need authorization to access info from endpoint
+    const response = await fetch(apiURL, {
+      method: 'GET',
+      // grants access to playlist endpoint
+      headers: { 'Authorization': `Bearer ${apiAuth}`} }).then((response) => {
+        response.json().then((data) => {
+          setPlaylistData(data.playlists.items)
+          
+        })
+      });
+  }
+  
+  // makes API call to store data in playlistData
+  useEffect(() => {getPlaylist()}, [])
+  console.log(playlistData)
+  // console.log(`playlists ${JSON.stringify(playlistData)}`)
+
+  const onNextPlaylist = () => {
     try {
-      // if (index == 25) {
-      //   // resets index once all playlists have been displayed
-      //   setIndex(0)
-      // } else {
-        let currentIndex = index;
-        // gets authorization token for API request
-        const apiAuth = "abcdefg";
-        console.log(`My API access ${apiAuth}`)
-        // Spotify API endpoint to rnb playlists
-        const apiURL = 'https://api.spotify.com/v1/search?q=genre%3AR%26b&type=playlist&limit=25'
-        // need authorization to access info from endpoint
-        const response = await fetch(apiURL, {
-          method: 'GET',
-          // grants access to playlist endpoint
-          headers: { 'Authorization': `Bearer ${apiAuth}`} });
-        const data = await response.json();
-        
-        // .playlist.items get information from API about each playlist
-        console.log(data.playlists.items);
-        // return data.playlists.items
-        setPlaylistName(data.playlists.items[currentIndex].name);
-        setPlaylistTracks(data.playlists.items[currentIndex].tracks.total);
-        setPlaylistOwner(data.playlists.items[currentIndex].owner.display_name);
-        setPlaylistImg(data.playlists.items[currentIndex].images[0].url);
-        
-        // updates index value on next click
-        setIndex(index+1);
-
+      let currentIndex = index;
+      if (index == 25) {
+        // resets index once all playlists have been displayed
+        setIndex(0)
+      } else {
+      // gets the specific information from the doc
+      setPlaylistName(playlistData[currentIndex].name);
+      setPlaylistTracks(playlistData[currentIndex].tracks.total);
+      setPlaylistOwner(playlistData[currentIndex].owner.display_name);
+      setPlaylistImg(playlistData[currentIndex].images[0].url);
+      
+      // updates index value on next click
+      setIndex(index+1);
+      }
     } catch (error) {
       console.log(error)
     }
   }
-
-  console.log(onNextPlaylist())
 
   return (
     <div className='App'>
